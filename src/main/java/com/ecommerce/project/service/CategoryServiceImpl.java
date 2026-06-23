52785f77f84504e6d920c020d6e016a144253c7a
rxjans <rajan4.sharma2001@gmail.com>
2026-06-23 20:10:51 +0530

    Implement: DTO pattern for DeleteCategory

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -47,11 +47,11 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public String deleteCategory(Long categoryId) {
+    public CategoryDTO deleteCategory(Long categoryId) {
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
         categoryRepository.delete(category);
-        return "Category with id: " + categoryId +  " Deleted Successfully";
+        return modelMapper.map(category, CategoryDTO.class);
     }
 
     @Override
